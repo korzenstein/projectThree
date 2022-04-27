0028cb6765510cac35c4c6d3031cb86aa24eb950
Stephen Korzenstein <skorzenstein@gmail.com>
2022-04-27 19:06:48 -0400

    project outline met with 4 apis

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
 
 const [location, setLocation] = useState("2092 Avenue Road, Toronto Canada")
 const [birdsArray, setBirdsArray] = useState([])
+const [weather, setWeather] = useState([])
 
 useEffect(() => {
-// Location API Call for Lat & Long
-
+// STEP 1 - Location API Call for Lat & Long
   const configLocale = {
   method: 'get',
   url: `http://api.positionstack.com/v1/forward`,
@@ -36,12 +36,14 @@ const getLocation = (data) => {
     let longitude = data.longitude.toFixed(2)
     console.log(latitude)
     console.log(longitude)
+    // STEP 2 - birdCall function
     birdCall(latitude, longitude)
+    // STEP 3 - weatherCall function
+    weatherCall(latitude, longitude)
 }
 
-// Bird API call for recent sitings
+// Bird API call for recent sitings, passing in lat and long
 const birdCall = (latitude, longitude) => {
-
 const configBird = {
   method: 'get',
   url: `https://api.ebird.org/v2/data/obs/geo/recent?lat=${latitude}&lng=${longitude}`,
@@ -61,14 +63,47 @@ axios(configBird)
 .catch(function (error) {
   console.log(error);
 });
+}
 
+// Weather API - for location
+const weatherCall = (latitude, longitude) => {
+  const APIkey = `61ff2ff2a45475c0d60c3ba5c2a56d10`;
+  const configWeath = {
+  method: 'get',
+  url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}`,
+};
+
+axios(configWeath)
+.then(function (response) {
+  console.log((response.data));
+  setWeather(response.data)
+})
+.catch(function (error) {
+  console.log(error);
+});
 }
 
 
+const getUnsplash = () => {
+  const query = 'House Finch'
+  const APIkey = 'nF_y6-AHiFcd560pleOqLVzuFpkZXAt5c4ZqQrcy_HU'
+  const configSplash = {
+  method: 'get',
+  url: `https://api.unsplash.com/search/photos?query=${query}&client_id=${APIkey}`,
+};
+
+axios(configSplash)
+.then(function (response) {
+  console.log((response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+}
+// getUnsplash()
+
 }, [])
-
-
-
 
 
   return (
@@ -79,3 +114,27 @@ axios(configBird)
 }
 
 export default App;
+
+
+// A landing page with the app heading 'Birds in Your Neighbourhood' and a welcome message asking users to search their location to find birds around them.
+
+// The page also contains an input field (text) for them to place their address. Once submitted, an API call (axios) is made using “Positionstack” to translate the users string location into numeric lat/long data that can be used for the other APIs. 
+
+// These will be stored as variables which will be passed into the “Cornell eBird" API that will make a call using to grab a set of recently spotted birds in the users area/search terms. 
+
+// Another API will be called to grab local weather data (“Openweather”) again using the lad/long variables. A fourth API ("Unsplash") will then obtain bird images to be passed into the cards.
+
+// The page will then display a selection (2-6) birds as cards. The cards will contain the following information on each, using the .map array method:
+
+// Name
+// Exact location
+// Scientific name
+// Image
+
+// Weather data that can help inform birders on their outdoor quests will be listed at the top of the page with the current:
+
+// Temp
+// Wind speed / direction
+// Visibility
+
+
